bac171a573f5281fb92aa15c7c2d11aa0255826d
Stephen Evina <stephenevina9@gmail.com>
2025-12-02 19:31:02 +0800

    stuck at chapter 9

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,20 +1,5 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices } from '@/app/lib/data';
+import DashboardSkeleton from '@/app/ui/skeletons';
  
-export default async function LatestInvoices() { // Remove props
-  const latestInvoices = await fetchLatestInvoices();
- 
-  return (
-    <div
-      className={clsx('flex items-center gap-2 p-4', lusitana.className)}
-      role="status"
-      aria-live="polite"
-    >
-      <ArrowPathIcon className="h-5 w-5 animate-spin" aria-hidden="true" />
-      <span>Loading latest invoices…</span>
-    </div>
-  );
+export default function Loading() {
+  return <DashboardSkeleton />;
 }
--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,17 +1,13 @@
-import CardWrapper from '@/app/ui/dashboard/cards';
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data'; // Remove fetchLatestInvoices
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import {
-  RevenueChartSkeleton,
-  LatestInvoicesSkeleton,
-  CardsSkeleton,} from '@/app/ui/skeletons';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
-  // Remove `const latestInvoices = await fetchLatestInvoices()`
+  const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -35,9 +31,10 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
         </Suspense>
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
