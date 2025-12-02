13c9825a026e0467b4360be923ca2b7d19cce361
Stephen Evina <stephenevina9@gmail.com>
2025-12-02 20:42:20 +0800

    stuck at action

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,6 +1,12 @@
 'use server';
  
 import { z } from 'zod';
+import postgres from 'postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
  
 const FormSchema = z.object({
   id: z.string(),
@@ -22,4 +28,13 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
+      await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  
 }
